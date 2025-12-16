cd4909c5db2491bf2a3cfe62787d72b6a3aab45c
Rami Sabbagh <rami.sab07@gmail.com>
2025-12-16 19:44:31 +0300

    Prepare the default file to start working with

---
+++ lectures/07/01-blending.cpp
@@ -0,0 +1,109 @@
+﻿// ---- تضمين المكاتب الخارجية ---- //
+
+#include <glad/glad.h>
+
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+// ---- تضمين المكاتب الاساسية ---- //
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+// ---- تضمين ترويسات المشروع ---- //
+
+#include "lab-utils/basic-shape.hpp"
+#include "lab-utils/orbit-application.hpp"
+
+// ---- قيم مساعدة ---- //
+
+// الألوان المساعدة أصبحت ضمن ترويسات المشروع
+#include "lab-utils/colors.hpp"
+// using namespace LabUtils::Colors;
+
+// ---- البرنامج ---- //
+
+class PracticeApplication : public LabUtils::LabOrbitApplication
+{
+	// ---- متحولات البرنامج ---- //
+
+	LabUtils::BasicShape axis;
+	LabUtils::BasicShape shape;
+
+	glm::mat4 transform = { 1.0f };
+
+	GLuint texture = 0;
+
+	// ---- مراحل حياة البرنامج ---- //
+
+	void onInit() override
+	{
+		createAxis();
+		using namespace LabUtils::Colors;
+
+		// -- تعريف الشكل -- //
+
+		shape = LabUtils::BasicShape({
+			// الموقع (x, y, z), اللون
+			{ {0.0f, 0.0f, 0.8f }, white }, // الزاوية العلوية
+			{ {0.0f, 0.5f, -0.5f}, white }, // الزاوية السفلية - اليمينية
+			{ {0.0f, -0.5f, -0.5f}, white }, // الزاوية السفلية - اليسارية
+			}, GL_TRIANGLES);
+	}
+
+	void onUpdate(float t, float dt) override
+	{
+
+	}
+
+	void onDraw(float t, float dt) override
+	{
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shape.render(transform, camera);
+
+		glLineWidth(3.0f);
+		glDepthFunc(GL_ALWAYS);
+		axis.render(glm::identity<glm::mat4>(), camera);
+		glDepthFunc(GL_LESS);
+	}
+
+	void onExit()
+	{
+		
+	}
+
+	// ---- توابع مساعدة ---- //
+
+	void createAxis()
+	{
+		axis = LabUtils::BasicShape({
+			// X Axis, Red
+			{{.0f, .0f, .0f}, { 1.f, 0.f, 0.f }},
+			{{.1f, .0f, .0f}, { 1.f, 0.f, 0.f }},
+
+			// Y Axis, Green
+			{{.0f, .0f, .0f}, { 0.f, 1.f, 0.f }},
+			{{.0f, .1f, .0f}, { 0.f, 1.f, 0.f }},
+
+			// Z Axis, Blue
+			{{.0f, .0f, .0f}, { 0.f, 0.f, 1.f }},
+			{{.0f, .0f, .1f}, { 0.f, 0.f, 1.f }},
+			}, GL_LINES);
+	}
+};
+
+// ==========--: نقطة دخول البرنامج :--========== //
+
+int main() {
+	PracticeApplication app;
+	app.run({ 600, 600 }, "Blending (L07:P01)");
+
+	return 0;
+}
