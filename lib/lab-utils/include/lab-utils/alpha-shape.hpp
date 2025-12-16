b6ee502baad35d38041b2da747cc08869790c2c0
Rami Sabbagh <rami.sab07@gmail.com>
2025-12-16 20:51:50 +0300

    Create AlphaShape based on BasicShape

---
+++ lib/lab-utils/include/lab-utils/alpha-shape.hpp
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+// ---- تضمين المكاتب الخارجية ---- //
+
+// من أجل استخدام اصدار حديث من الأوبن جي ال
+// لا بد من استخدام مكتبة وسيطة
+// GLAD: https://github.com/Dav1dde/glad, https://glad.dav1d.de/
+// المرجع: https://docs.gl/ أو https://devdocs.io/
+#include <glad/glad.h>
+
+// للتعامل مع الأشعة والمصفوفات الرياضية
+// وانشاء مصفوفات تحويلات هندسية وإسقاط
+// GLM: OpenGL Mathematics: https://github.com/g-truc/glm/
+// دليل المستخدم: https://github.com/g-truc/glm/blob/master/manual.md
+// لربما من الأحسن الإعتماد على التتمة التلقائية أثناء الكتابة،
+// أو قراءة ملفات التوريس مباشرة.
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+namespace LabUtils
+{
+	struct AlphaVertex
+	{
+		glm::vec3 position;
+		glm::vec3 color;
+	};
+
+	class AlphaShape
+	{
+	protected:
+		int verticesCount = 0;
+
+		GLuint cameraLocation = 0, transformLocation = 0;
+		GLuint VAO = 0, VBO = 0;
+
+		GLenum drawMode = GL_TRIANGLES;
+
+	public:
+		static GLuint shaderProgram;
+		static void compileShapeShader();
+
+		AlphaShape();
+		AlphaShape(const std::vector<AlphaVertex>& vertices, GLenum drawMode = GL_TRIANGLES);
+		~AlphaShape();
+
+		AlphaShape& operator=(AlphaShape&& other) noexcept;
+
+		void render(const glm::mat4& transform = { 1.0f }, const glm::mat4& camera = { 1.0f }) const;
+	};
+}
---
+++ lib/lab-utils/src/alpha-shape.cpp
@@ -0,0 +1,146 @@
+﻿// عزيزي الطالب، هذا ملف مثال، ولك كامل الحرية في تعديله
+// ذكرت الغاية والمراجع لجميع المكاتب المستخدمة.
+
+// ---- تضمين المكاتب الخارجية ---- //
+
+// من أجل استخدام اصدار حديث من الأوبن جي ال
+// لا بد من استخدام مكتبة وسيطة
+// GLAD: https://github.com/Dav1dde/glad, https://glad.dav1d.de/
+// المرجع: https://docs.gl/ أو https://devdocs.io/
+#include <glad/glad.h>
+
+// ---- تضمين المكاتب الاساسية ---- //
+
+#include <iostream>
+#include <string>
+
+// ---- تضمين ترويسات المشروع ---- //
+
+#include "lab-utils/shader-utils.hpp"
+#include "lab-utils/alpha-shape.hpp"
+
+// ---- الشيدر البسيط للرسم من دون إضاءة ---- //
+
+static const char* ALPHA_VERTEX_SHADER_SRC = R"(
+#version 330 core
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec3 aColor;
+
+out vec3 vertexColor;
+
+uniform mat4 camera;
+uniform mat4 transform;
+
+void main() {
+gl_Position = camera * transform * vec4(aPos, 1.0);
+vertexColor = aColor;
+}
+)";
+
+static const char* ALPHA_FRAMGENT_SHADER_SRC = R"(
+#version 330 core
+
+in vec3 vertexColor;
+
+out vec4 FragColor;
+
+void main() {
+FragColor = vec4(vertexColor, 1.0);
+}
+)";
+
+// ---- الصف المساعد لرسم الأشكاء ---- //
+
+namespace LabUtils
+{
+	GLuint AlphaShape::shaderProgram = 0;
+
+	void AlphaShape::compileShapeShader()
+	{
+		if (shaderProgram != 0) return; // Shader already compiled.
+		shaderProgram = compileAndLinkShaderProgram(ALPHA_VERTEX_SHADER_SRC, ALPHA_FRAMGENT_SHADER_SRC, "basic-shader");
+	}
+
+	AlphaShape::AlphaShape()
+	{
+		verticesCount = 0;
+	}
+
+	AlphaShape::AlphaShape(const std::vector<AlphaVertex>& vertices, GLenum drawMode)
+	{
+		verticesCount = vertices.size();
+		if (verticesCount == 0) return; // Don't render empty shapes.
+
+		this->drawMode = drawMode;
+
+		compileShapeShader(); // Ensure the shader is compiled.
+
+		// Upload Vertex Data
+		int size = verticesCount * sizeof(AlphaVertex);
+
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, size, vertices.data(), GL_STATIC_DRAW);
+
+		// Configure Attributes
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+		// - Position
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(AlphaVertex), (void*)(offsetof(AlphaVertex, position)));
+		glEnableVertexAttribArray(0);
+		// - Color
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(AlphaVertex), (void*)(offsetof(AlphaVertex, color)));
+		glEnableVertexAttribArray(1);
+
+		// Get Uniforms Locations
+		cameraLocation = glGetUniformLocation(shaderProgram, "camera");
+		transformLocation = glGetUniformLocation(shaderProgram, "transform");
+	}
+
+	AlphaShape::~AlphaShape()
+	{
+		if (verticesCount == 0) return;
+
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteProgram(shaderProgram);
+
+		verticesCount = 0;
+	}
+
+	AlphaShape& AlphaShape::operator=(AlphaShape&& other) noexcept
+	{
+		if (this != &other)
+		{
+			// free existing resources
+			if (verticesCount != 0) this->~AlphaShape();
+
+			// steal resources
+			verticesCount = other.verticesCount;
+			shaderProgram = other.shaderProgram;
+			cameraLocation = other.cameraLocation;
+			transformLocation = other.transformLocation;
+			VAO = other.VAO;
+			VBO = other.VBO;
+
+			drawMode = other.drawMode;
+
+			// leave other in null state
+			other.verticesCount = 0;
+		}
+
+		return *this;
+	}
+
+	void AlphaShape::render(const glm::mat4& transform, const glm::mat4& camera) const
+	{
+		if (verticesCount == 0) return;
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glUniformMatrix4fv(cameraLocation, 1, false, glm::value_ptr(camera));
+		glUniformMatrix4fv(transformLocation, 1, false, glm::value_ptr(transform));
+		glDrawArrays(drawMode, 0, verticesCount);
+	}
+}
+
